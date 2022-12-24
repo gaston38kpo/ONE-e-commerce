e129aef85e3746f73ff89fd8b509b3765f226f50
Gaston Giacobini <lu0210035@gmail.com>
2022-12-24 01:26:10 -0300

    fix url user

--- assets/js/services/user-service.js
+++ assets/js/services/user-service.js
@@ -1,14 +1,14 @@
 const listUsers = async () => {
-  const response = await fetch("http://127.0.0.1:3000/users/");
-  // const response = await fetch("https://e-commerce-lz5p.onrender.com/users/");
+  // const response = await fetch("http://127.0.0.1:3000/users/");
+  const response = await fetch("https://e-commerce-lz5p.onrender.com/users/");
   return await response.json();
 };
 
 const readUser = async (id) => {
   try {
     const response = await fetch(
-      `http://127.0.0.1:3000/users/${id}`
-      // `https://e-commerce-lz5p.onrender.com/users/${id}`
+      // `http://127.0.0.1:3000/users/${id}`
+      `https://e-commerce-lz5p.onrender.com/users/${id}`
     );
     return await response.json();
   } catch (error) {
